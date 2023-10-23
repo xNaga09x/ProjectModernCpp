556ea64a360be58f50f2e884a1448390e54094df
Pavel Andrei <andrei-va.pavel@student.unitbv.ro>
2023-10-23 20:00:05 +0300

    Stopwatch V1 WV(Working Version)

---
+++ Project/Stopwatch_test.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <Windows.h>
+#include <cstdlib>
+
+void stopwatch()
+{
+	short seconds = 60;
+
+	for (int i = 60; i >= 0; i--)
+	{
+		system("cls");
+		std::cout << "Time remaining:" << seconds;
+		--seconds;
+		Sleep(1000); //1000ms = 1s
+	}
+	system("cls");
+	std::cout << "Round finished!";
+}
+
+int main()
+{
+	stopwatch();
+	return 0;
+}
