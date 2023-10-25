021e8ac3a8343793f964caef7d5b5920baf650a9
Tomescu-Andrei <andrei-cristian.tomescu@student.unitbv.ro>
2023-10-25 22:15:05 +0300

    Class Round

---
+++ Project/Round.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Game.h"
+#include "User.h"
+
+class Round:
+	public Game
+{
+private:
+	int noRounds;
+	static const int noSubrounds = Players.size();
+	//constructori
+
+};
+
