34e5beac1a74315ac7450851c86c7c908253a102
Tomescu-Andrei <andrei-cristian.tomescu@student.unitbv.ro>
2023-11-04 18:53:46 +0200

    Created Rule of Five for Round

--- Round.cpp
+++ Round.cpp
@@ -12,3 +12,36 @@ Round::Round(int noRounds)
 	:m_noRounds(noRounds)
 {
 }
+
+round::Round::Round(const Round& other)
+	: m_noRounds(other.m_noRounds)
+{
+}
+
+round::Round::Round(Round&& other) noexcept
+	: m_noRounds(other.m_noRounds)
+{
+	other.m_noRounds = 0;
+}
+
+Round& round::Round::operator=(Round&& other) noexcept
+{
+    if(this != &other) {
+        m_noRounds = other.m_noRounds;
+        other.m_noRounds = 0;
+    }
+    return *this;
+}
+
+Round& round::Round::operator=(const Round& other)
+{
+    if (this != &other) {
+        m_noRounds = other.m_noRounds;
+    }
+    return *this;
+}
+
+
+
+
+
