887c91d503e62e5a28b801179930da8d73452b6a
VulpoiuPetru <petru.vulpoiu@student.unitbv.ro>
2023-11-04 18:37:28 +0200

    Create Rule of Five for Subround

--- Subround.cpp
+++ Subround.cpp
@@ -12,3 +12,31 @@ Subround::Subround(const user::User& painter, const std::vector<user::User>& gue
 	: m_painter(painter), m_guessers(guessers)
 {
 }
+
+Subround& subround::Subround::operator=(const Subround& other)
+{
+	if (this == &other) {
+		return *this;
+	}
+	round::Round::operator=(other);
+	m_painter = other.m_painter;
+	m_guessers = other.m_guessers;
+	return *this;
+}
+
+subround::Subround::Subround(Subround&& other) noexcept
+{
+	m_painter = std::move(other.m_painter);
+	m_guessers = std::move(other.m_guessers);
+}
+
+Subround& subround::Subround::operator=(Subround&& other) noexcept
+{
+	if (this == &other) {
+		return *this;
+	}
+	round::Round::operator=(std::move(other));
+	m_painter = std::move(other.m_painter);
+	m_guessers = std::move(other.m_guessers);
+	return *this;
+}
--- Subround.cppm
+++ Subround.cppm
@@ -14,5 +14,9 @@ namespace subround {
         Subround();
 
         Subround(const user::User& m_painter, const std::vector<user::User>& m_guessers);
+
+        Subround& operator=(const Subround& other);//copy assigment
+        Subround(Subround&& other) noexcept;//move constructor
+        Subround& operator=(Subround&& other) noexcept;//move assigment
 	};
 }
