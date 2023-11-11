7bea8669eee25fe657c714a0ebbfff19cc2537b0
Sergiu Onofrei <sergiu.onofrei@student.unitbv.ro>
2023-11-11 18:31:44 +0200

    StopWatch_test.cpp deleted

---
+++ Project/StopWatch.h
@@ -0,0 +1,29 @@
+#pragma once
+#include <iostream>
+#include <chrono>
+#include <thread>
+namespace gartic{
+class Stopwatch {
+private:
+    std::chrono::high_resolution_clock::time_point startTime;
+    std::chrono::high_resolution_clock::time_point endTime;
+
+public:
+    Stopwatch(){}
+    void start() 
+    {
+        startTime = std::chrono::high_resolution_clock::now();
+    }
+
+    void stop() 
+    {
+        endTime = std::chrono::high_resolution_clock::now();
+    }
+
+    double elapsed_time() 
+    {
+        std::chrono::duration<double> duration = endTime - startTime;
+        return duration.count();
+    }
+};
+}
