f7d378ac2562d0ff4ea8d40082d4921a0aca5342
Tomescu-Andrei <andrei-cristian.tomescu@student.unitbv.ro>
2023-11-12 17:15:50 +0200

    Created Database for Users WV

---
+++ Project/usersDB.h
@@ -0,0 +1,28 @@
+#pragma once
+module user;
+
+#include <string>
+#include <vector>
+
+#include <crow.h>
+#include <sqlite_orm/sqlite_orm.h>
+namespace sql = sqlite_orm;
+
+inline auto createStorage(const std::string& filename)
+{
+	return sql::make_storage(
+		filename,
+		sql::make_table(
+			"Users",
+			sql::make_column("id",&User::m_id),
+			sql::make_column("name",&User::m_name),
+			sql::make_column("average",&User::m_historyAverage)
+			)
+
+		);
+
+
+
+}
+using Storage = decltype(createStorage(""));
+//void populateStorage(Storage& storage);
