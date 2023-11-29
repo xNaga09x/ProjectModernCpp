7af7b93f8e238974c3d399ef2e67e00443985c16
Pavel Andrei <andrei-va.pavel@student.unitbv.ro>
2023-11-29 15:43:06 +0200

    Finished Draw Area, working on the MainWindow class of Project Draw, not completed NW

--- Draw/DrawArea.cpp
+++ Draw/DrawArea.cpp
@@ -33,7 +33,8 @@ bool DrawArea::saveImage(const QString& fileName, const char* fileFormat)
     QImage visibleImage = image;
     resizeImage(&visibleImage, size());
 
-    if (visibleImage.save(fileName, fileFormat)) {
+    if (visibleImage.save(fileName, fileFormat)) 
+    {
         modified = false;
         return true;
     }
@@ -58,4 +59,103 @@ void DrawArea::clearImage()
     image.fill(qRgb(255, 255, 255));
     modified = true;
     update();
+}
+
+void DrawArea::mousePressEvent(QMouseEvent* event)
+{
+    //verificam ce buton s-a apasat si daca este click stanga setam scribbling drept true
+    //tinem minte pozitia
+    if (event->button() == Qt::LeftButton) 
+    {
+        lastPoint = event->position().toPoint();
+        scribbling = true;
+    }
+}
+
+void DrawArea::mouseMoveEvent(QMouseEvent* event)
+{
+    //deseneaza linia cat timp este apasat butonul
+    if ((event->buttons() & Qt::LeftButton) && scribbling)
+        drawLineTo(event->position().toPoint());
+}
+
+void DrawArea::mouseReleaseEvent(QMouseEvent* event)
+{
+    //se opreste desenarea
+    if (event->button() == Qt::LeftButton && scribbling) {
+        drawLineTo(event->position().toPoint());
+        scribbling = false;
+    }
+}
+
+void DrawArea::paintEvent(QPaintEvent* event)
+{
+    //Cream un obiect de tip QPainter pentru zona de desenare si desenam
+    QPainter painter(this);
+    QRect dirtyRect = event->rect();
+    painter.drawImage(dirtyRect, image, dirtyRect);
+}
+
+void DrawArea::resizeEvent(QResizeEvent* event)
+{
+    //Redimensionam imaginea
+    if (width() > image.width() || height() > image.height()) {
+        int newWidth = qMax(width() + 128, image.width());
+        int newHeight = qMax(height() + 128, image.height());
+        resizeImage(&image, QSize(newWidth, newHeight));
+        update();
+    }
+    QWidget::resizeEvent(event);
+}
+
+void DrawArea::drawLineTo(const QPoint& endPoint)
+{
+    //Desenam o linie de la ultima pozitie pana la pozitia curenta
+    //Setam modificat ca true
+    //Resetam ultima pozitie pentru a continua din punctul in care ne-am oprit
+    //Updatam imaginea
+    QPainter painter(&image);
+    painter.setPen(QPen(myPenColor, myPenWidth, Qt::SolidLine, Qt::RoundCap,
+        Qt::RoundJoin));
+    painter.drawLine(lastPoint, endPoint);
+    modified = true;
+
+    int rad = (myPenWidth / 2) + 2;
+    update(QRect(lastPoint, endPoint).normalized()
+        .adjusted(-rad, -rad, +rad, +rad));
+    lastPoint = endPoint;
+}
+
+void DrawArea::resizeImage(QImage* image, const QSize& newSize)
+{
+    //Pentru redimensionarea imaginii cream o noua imagine alba cu dimensiunea dorita si uploadam vechea imagine 
+    if (image->size() == newSize)
+        return;
+
+    QImage newImage(newSize, QImage::Format_RGB32);
+    newImage.fill(qRgb(255, 255, 255));
+    QPainter painter(&newImage);
+    painter.drawImage(QPoint(0, 0), *image);
+    *image = newImage;
+}
+
+void DrawArea::print()
+{
+    //Cream un obiect QPrinter HR pt output
+    //In QDialog specificam dimensiunile dorite
+    //Daca este acceptata dimensiunea incepem desenarea
+#if defined(QT_PRINTSUPPORT_LIB) && QT_CONFIG(printdialog)
+    QPrinter printer(QPrinter::HighResolution);
+
+    QPrintDialog printDialog(&printer, this);
+    if (printDialog.exec() == QDialog::Accepted) {
+        QPainter painter(&printer);
+        QRect rect = painter.viewport();
+        QSize size = image.size();
+        size.scale(rect.size(), Qt::KeepAspectRatio);
+        painter.setViewport(rect.x(), rect.y(), size.width(), size.height());
+        painter.setWindow(image.rect());
+        painter.drawImage(0, 0, image);
+    }
+#endif // QT_CONFIG(printdialog)
 }
--- Draw/DrawArea.h
+++ Draw/DrawArea.h
@@ -1,6 +1,8 @@
 #pragma once
 
 #include <QMainWindow>
+#include <qevent.h>
+#include <qpainter.h>
 #include "ui_DrawArea.h"
 
 class DrawArea : public QMainWindow
@@ -11,6 +13,37 @@ public:
 	DrawArea(QWidget *parent = nullptr);
 	~DrawArea();
 
+    bool openImage(const QString& fileName);                         //incarca o imagine din fisier, poate fi modificata
+    bool saveImage(const QString& fileName, const char* fileFormat); //salveaza imaginea curenta intr-un fisier
+    void setPenColor(const QColor& newColor);                        
+    void setPenWidth(int newWidth);
+
+    bool isModified() const { return modified; }    // verifica daca s-a modificat ceva fata de starea anterioara
+    QColor penColor() const { return myPenColor; }  // tine minte culoarea selectata
+    int penWidth() const { return myPenWidth; }     // tine minte grosimea selectata
+
+public slots:
+    void clearImage();  //sterge imaginea curenta
+    void print();       //afisarea imaginii curente
+
+protected:
+    void mousePressEvent(QMouseEvent* event) override;  //
+    void mouseMoveEvent(QMouseEvent* event) override;   // aceste 3 functii au rolul de a implementa desenul
+    void mouseReleaseEvent(QMouseEvent* event) override;//
+
+    void paintEvent(QPaintEvent* event) override;       // pt. updatarea desenului
+    void resizeEvent(QResizeEvent* event) override;     // verif. ca QImaginea pe care desenam sa fie minim de dimensiunea widgetului
+
 private:
-	Ui::DrawAreaClass ui;
+    Ui::DrawAreaClass ui;
+
+    void drawLineTo(const QPoint& endPoint);                // excuta actiunea efectiva de desenare
+    void resizeImage(QImage* image, const QSize& newSize);  // schimba marimile imaginii
+
+    bool modified = false;
+    bool scribbling = false;        // este true daca se tine apasat click in zona de desenare
+    int myPenWidth = 1;
+    QColor myPenColor = Qt::blue;
+    QImage image;                   // contine imaginea desenata
+    QPoint lastPoint;               // tine minte ultima pozitie a unei actiuni a mouse-ului
 };
