457958e5aa49a7287bbfddff5b86d46102a1891d
Sergiu Onofrei <sergiu.onofrei@student.unitbv.ro>
2023-12-06 14:50:05 +0200

    Added functionality to chat window

--- Interface/Guess_Game_Interface.h
+++ Interface/Guess_Game_Interface.h
@@ -2,6 +2,10 @@
 
 #include <QMainWindow>
 #include "ui_Guess_Game_Interface.h"
+#include <QTextEdit>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QVBoxLayout>
 
 class Guess_Game_Interface : public QMainWindow
 {
@@ -11,8 +15,11 @@ public:
 	Guess_Game_Interface(QWidget *parent = nullptr);
 	~Guess_Game_Interface();
 
+private slots:
+	void sendMessage();
 
 
 private:
 	Ui::Guess_Game_InterfaceClass ui;
-};
+
+}; 
