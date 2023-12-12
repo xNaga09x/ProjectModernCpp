3d5dfdce6e9d2255af12dddd852e418294e44bc6
Sergiu Onofrei <sergiu.onofrei@student.unitbv.ro>
2023-12-12 18:52:49 +0200

    When loging in no longer stacks existing names in db

--- User/User/User.cpp
+++ User/User/User.cpp
@@ -1,39 +1,46 @@
 #include "User.h"
 
-User::User(QWidget *parent)
-    : QMainWindow(parent)
+User::User(QWidget* parent)
+	: QMainWindow(parent)
 {
-    ui.setupUi(this);
-    Lobby = nullptr;
+	ui.setupUi(this);
+	Lobby = nullptr;
 }
 
 User::~User()
 {}
 std::string User::getName() const
 {
-    return this->name;
+	return this->name;
 }
 
 void User::on_Loggin_clicked()
 {
-    //posibil de verificat daca apasa pe butonul de register/login sa se vada daca exista acel user , daca nu sa se verifice cand se da register
-    //daca exista in database , daca nu , se creeaza in database . Daca nu verifica nimeni , suntem safe.
-    this->name = ui.lineEdit->text().toStdString();
 
-    std::string json_data = this->name;
+	this->name = ui.lineEdit->text().toStdString();
+	auto usersResponse = cpr::Get(cpr::Url{ "http://localhost:18080/users" });
+	std::string json_data = this->name;
 
-    auto response = cpr::Post(
-        cpr::Url{ "http://localhost:18080/adduser" },
-        cpr::Body(json_data)
-    );
+	crow::json::rvalue usersJson = crow::json::load(usersResponse.text);
+	
+	auto found = usersResponse.text.find(this->name);
 
-    this->close();
-    Lobby = new Lobby_Interface(this);
-    Lobby->show();
+	if(found==std::string::npos)
+	{
+		auto response = cpr::Post(
+			cpr::Url{ "http://localhost:18080/adduser" },
+			cpr::Body(json_data)
+		);
+	}
+
+	this->close();
+	Lobby = new Lobby_Interface(this);
+	Lobby->show();
 }
+
 void User::on_Register_clicked()
 {
-    this->close();
-    Lobby = new Lobby_Interface(this);
-    Lobby->show();
+	this->close();
+	Lobby = new Lobby_Interface(this);
+	Lobby->show();
 }
